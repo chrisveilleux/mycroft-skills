129ed5977ac81daf48d149d681f848d2aa86e798
Chris Veilleux <veilleux.chris@gmail.com>
2020-03-17 15:31:59 -0500

    debug

--- voight_kampff.py
+++ voight_kampff.py
@@ -25,6 +25,7 @@ pr = repo.get_pull(pr_number)
 
 # Determine if the PR is for a new or updated skill by inspecting the PR diff
 pr_diff = requests.get(pr.diff_url)
+print(pr_diff)
 diff_file_name = None
 skill_submodule_name = None
 for line in pr_diff.text.split('\n'):
