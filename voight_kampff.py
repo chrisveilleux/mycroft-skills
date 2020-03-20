e5ab9d2e30b80b42c9165c6f2557b76a1d1af33b
Chris Veilleux <veilleux.chris@gmail.com>
2020-03-19 20:49:20 -0500

    Revert "change script to build a config file for input into test setup script"
    This reverts commit becf5bb3

--- voight_kampff.py
+++ voight_kampff.py
@@ -38,9 +38,7 @@ for line in pr_diff.text.split('\n'):
         skill_submodule_name = diff_file_name
         break
 
-with open('skill_test.yaml', 'w') as config_file:
-    config_file.write('tested_skills:\n')
-    config_file.write('- ' + skill_submodule_name + '\n')
+print('export SKILL_NAME={}'.format(skill_submodule_name))
 
 # # There are files in the repository that are not skill subprojects.
 # # No need to run the tests if a skill is not changed.
